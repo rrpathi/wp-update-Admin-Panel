efa569c88094f96c06dc9c2ddad6f0703d03862a
Ragupathi\ragu <ragu3610@gmail.com>
2018-10-31 01:44:29 +0530

    update notification via date

---
+++ controller/wp_update.php
@@ -0,0 +1,15 @@
+<?php 
+	include_once '../model/index.php';
+	global $wpdb;
+	if(!empty($_POST['plugin_communication_key'])){
+		$plugin_communication_key = $_POST['plugin_communication_key'];
+	$date_validation = $wpdb->get_results("SELECT * FROM activation_key where plugin_communication_key ='$plugin_communication_key' ",ARRAY_A);
+	$timeDiff = strtotime($date_validation[0]['to_date'])-(strtotime(date('d-m-Y')));
+	$remaining_date = $timeDiff/86400;
+		if($remaining_date >=1){
+			echo json_encode(array('slug'=>'wp-form','download_url'=>'http://localhost/wp-form.zip','version'=>'3.0'));
+		}
+	}
+ ?>
+
+ 
