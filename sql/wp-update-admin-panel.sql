d4380bfe21f195aae46a0a963c216a241bde7d13
Ragupathi\ragu <ragu3610@gmail.com>
2018-10-31 03:35:45 +0530

    sql file change

--- sql/wp-update-admin-panel.sql
+++ sql/wp-update-admin-panel.sql
@@ -1,4 +1,4 @@
--- Adminer 4.6.2 MySQL dump
+-- Adminer 4.3.1 MySQL dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -16,12 +16,23 @@ CREATE TABLE `activation_key` (
   `activation_key` varchar(100) NOT NULL,
   `site_url` varchar(200) DEFAULT NULL,
   `activation_key_status` int(11) DEFAULT '0',
+  `plugin_communication_key` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `activation_key` (`id`, `name`, `email`, `mobile_number`, `from_date`, `to_date`, `activation_key`, `site_url`, `activation_key_status`) VALUES
-(1,	'Ragupathi',	'ragu3610@gmail.com',	8526462226,	'30-10-2018',	'31-10-2018',	'a8fe8f196ffe417aefc3c814b452e76afbf5c3a7',	NULL,	0),
-(2,	'xcvxcv',	'xcvxcvx@xfdcxfg',	8526462227,	'30-10-2018',	'31-10-2018',	'54b4810e1e83c1d795ff5a19d6d375702277e4f6',	NULL,	0),
-(3,	'ragu',	'ragu3610@gmail.com',	8526462226,	'30-10-2018',	'31-10-2018',	'e028102fd161a55b6f8b5115dcb6440ddbbb10ba',	NULL,	0);
+INSERT INTO `activation_key` (`id`, `name`, `email`, `mobile_number`, `from_date`, `to_date`, `activation_key`, `site_url`, `activation_key_status`, `plugin_communication_key`) VALUES
+(1,	'Ragupathi',	'ragu3610@gmail.com',	8526462226,	'31-10-2018',	'31-10-2018',	'9369eb6cbf5aea6505c7f48d3a8a7128fa297331',	'http://localhost/wp-update',	1,	'1120218fb728701100693613e328f6a439972493');
 
--- 2018-10-30 13:36:41
+DROP TABLE IF EXISTS `wordpress_release`;
+CREATE TABLE `wordpress_release` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `slug` varchar(100) NOT NULL DEFAULT 'wp_form',
+  `download_url` varchar(100) NOT NULL DEFAULT 'http://localhost/wp-form.zip',
+  `version` varchar(100) NOT NULL DEFAULT '2.0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `wordpress_release` (`id`, `slug`, `download_url`, `version`) VALUES
+(1,	'wp_form',	'http://localhost/wp-form.zip',	'3.0');
+
+-- 2018-10-30 22:03:57
