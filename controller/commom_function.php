216decccf9670b49f8b47bc7ea45d4a6f26bc8f2
Ragupathi <ragu3610@gmail.com>
2018-10-31 15:13:50 +0530

    change smtp official server

--- controller/commom_function.php
+++ controller/commom_function.php
@@ -8,14 +8,14 @@ function send_mail($to,$activation_key){
 	try {
 	// $mail->SMTPDebug = 2;                                 // Enable verbose debug output
 		$mail->isSMTP();                                      // Set mailer to use SMTP
-		$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+		$mail->Host = 'mail.ragupathi.rxforge.in';  // Specify main and backup SMTP servers
 		$mail->SMTPAuth = true;                               // Enable SMTP authentication
-		$mail->Username = 'ragupathi@revmakx.com';                 // SMTP username
+		$mail->Username = 'ragupathi@ragupathi.rxforge.in';                 // SMTP username
 		$mail->Password = 'sollamudiyadhu';                           // SMTP password
 		$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 		$mail->Port = 587;                                    // TCP port to connect to
 		$mail->addAddress($to,'user');     // To Address
-		$mail->setFrom('ragupathi@revmakx.com', 'Mailer');			 //From Address	
+		$mail->setFrom('ragupathi@ragupathi.rxforge.in', 'Mailer');			 //From Address	
 		$mail->isHTML(true);                                  // Set email format to HTML
 		$mail->Subject = 'Welcome to Revmax Technologies Your Activation Code';
 		$mail->Body    = 'Thanks Purchase Order Your Activation Code: <b style="color:green;">'.$activation_key.'</b>';
